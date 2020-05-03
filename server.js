9641bad4a2f812b5c29e436d5e4f9bca757a0e47
ashrafhussain17 <ashrafhussain.2317.csedu@gmail.com>
2020-05-04 01:04:46 +0600

    backend done and frontent done with html

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const stripe = require('stripe')('sk_test_gM1Pz3AgOqPyiVRrKIJbAdIQ00lR1hhpux');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, './views')));
+
+app.post("/charge", (req, res) => {
+    try {
+        stripe.customers
+            .create({
+                name: req.body.name,
+                email: req.body.email,
+                source: req.body.stripeToken
+            })
+            .then(customer =>
+                stripe.charges.create({
+                    amount: req.body.amount * 100,
+                    currency: "usd",
+                    customer: customer.id
+                })
+            )
+            .then(() => res.render("completed.html"))
+            .catch(err => console.log(err));
+    } catch (err) {
+        res.send(err);
+    }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Server is running...'));
---
+++ views/card.js
@@ -0,0 +1,35 @@
+const stripe = Stripe('pk_test_5OHEN6ejR1Dpmy3NKO7EkOt100DNAAvxQ7');
+const elements = stripe.elements();
+
+var style = {
+  base: {
+    color: "#fff"
+  }
+};
+
+const card = elements.create('card', { style });
+card.mount('#card-element');
+
+const form = document.querySelector('form');
+const errorEl = document.querySelector('#card-errors');
+
+// Give our token to our form
+const stripeTokenHandler = token => {
+  const hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  form.submit();
+}
+
+// Create token from card data
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  stripe.createToken(card).then(res => {
+    if (res.error) errorEl.textContent = res.error.message;
+    else stripeTokenHandler(res.token);
+  })
+})
